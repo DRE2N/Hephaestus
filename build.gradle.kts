0609f673fe91e1a5216487e91705e15e4f5e6c2b
Malfrador <malfrador@gmail.com>
2024-07-10 19:59:33 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+
+import java.io.FileInputStream
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://erethon.de/repo")
+}
+plugins {
+    `java-library`
+    `maven-publish`
+    id("io.papermc.paperweight.userdev") version "1.7.1"
+    id("xyz.jpenilla.run-paper") version "1.0.6" // Adds runServer and runMojangMappedServer tasks for testing
+}
+
+group = "de.erethon"
+version = "1.0-SNAPSHOT"
+description = "Items"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+val papyrusVersion = "1.21-R0.1-SNAPSHOT"
+
+dependencies {
+    paperweight.devBundle("de.erethon.papyrus", papyrusVersion) { isChanging = true}
+}
+
+tasks {
+    paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
+    runServer {
+        if (!project.buildDir.exists()) {
+            project.buildDir.mkdir()
+        }
+        val f = File(project.buildDir, "server.jar");
+        uri("https://github.com/DRE2N/Papyrus/releases/download/latest/papyrus-paperclip-$papyrusVersion-reobf.jar").toURL().openStream().use { it.copyTo(f.outputStream()) }
+        serverJar(f)
+    }
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(21)
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    jar {
+        manifest {
+            attributes(
+                "paperweight-mappings-namespace" to "mojang"
+            )
+        }
+    }
+}
---
+++ src/main/java/de/erethon/hephaestus/Hephaestus.java
@@ -0,0 +1,17 @@
+package de.erethon.hephaestus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hephaestus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
